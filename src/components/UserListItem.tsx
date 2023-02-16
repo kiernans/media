be1bbd9c8eaba49e6f18ef6805bfefcc6ad2f38e
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-02-16 15:12:46 -0700

    Create ExpandablePanel and AlbumsList components

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -3,6 +3,8 @@ import { GoTrashcan } from 'react-icons/go';
 import Button from './Button';
 import { deleteUser } from '../store';
 import { useThunk } from '../hooks/useThunk';
+import ExpandablePanel from './ExpandablePanel';
+import AlbumsList from './AlbumsList';
 
 type UserListItemProps = {
 	user: User;
@@ -17,18 +19,20 @@ const UserListItem = ({ user }: UserListItemProps) => {
 		}
 	};
 
+	const header = (
+		<>
+			<Button loading={isLoading as boolean} onClick={handleClick}>
+				<GoTrashcan />
+			</Button>
+			{error && <div>Error deleting user.</div>}
+			{user.name}
+		</>
+	);
+
 	return (
-		<div className='mb-2 border rounded'>
-			<div className='flex p-2 justify-between items-center cursor-pointer'>
-				<div className='flex flex-row items-center justify-between'>
-					<Button loading={isLoading as boolean} onClick={handleClick}>
-						<GoTrashcan />
-					</Button>
-					{error && <div>Error deleting user.</div>}
-					{user.name}
-				</div>
-			</div>
-		</div>
+		<ExpandablePanel header={header}>
+			<AlbumsList user={user} />
+		</ExpandablePanel>
 	);
 };
 
