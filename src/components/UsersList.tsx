93fa82eec4e317c5e8f5b6b5a0e25610f240d224
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-02-16 14:05:33 -0700

    Add functionality for loading and creating users

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,5 +1,62 @@
+import { useEffect } from 'react';
+import { useAppSelector } from '../hooks/hooks';
+import { fetchUsers, addUser } from '../store';
+import Skeleton from './Skeleton';
+import Button from './Button';
+import { useThunk } from '../hooks/useThunk';
+
 const UsersList = () => {
-	return <div>UsersList</div>;
+	const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+		useThunk(fetchUsers);
+	const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+	const { data } = useAppSelector((state) => {
+		return state.users;
+	});
+
+	useEffect(() => {
+		if (typeof doFetchUsers === 'function') {
+			doFetchUsers();
+		}
+	}, [doFetchUsers]);
+
+	const handleUserAdd = () => {
+		if (typeof doCreateUser === 'function') {
+			doCreateUser();
+		}
+	};
+
+	let content;
+
+	if (isLoadingUsers) {
+		content = <Skeleton times={6} className='h-10 w-full' />;
+	} else if (loadingUsersError) {
+		content = <div>Error fetching data...</div>;
+	} else {
+		content = data.map((user) => {
+			return (
+				<div key={user.id} className='mb-2 border rounded'>
+					<div className='flex p-2 justify-between items-center cursor-pointer'>
+						{user.name}
+					</div>
+				</div>
+			);
+		});
+	}
+
+	return (
+		<div>
+			<div className='flex flex-row justify-between items-center m-3'>
+				<h1 className='m-2 text-xl'>Users</h1>
+
+				<Button loading={isCreatingUser as boolean} onClick={handleUserAdd}>
+					+ Add User
+				</Button>
+
+				{creatingUserError && 'Error creating user...'}
+			</div>
+			{content}
+		</div>
+	);
 };
 
 export default UsersList;
